fd94b996866bcca85e8e1637c73de1155c2a9b45
esaumusic <12537635+esaumusic@users.noreply.github.com>
2018-07-25 22:07:30 -0700

    Update ding-pi.py

--- ding-pi.py
+++ ding-pi.py
@@ -1,5 +1,5 @@
 # 07-23-18 - ESAU's DingPi Script (no this isn't chinese food, this turns your raspberry pi 3 into a midi usb host/router/midi processor by way of MidiDings!)
-#
+
 # Prerequisites:
 # Raspberry Pi 3/3+ (other models untested but might work)
 # Installed Raspbian Stretch Lite (https://www.raspberrypi.org/downloads/raspbian/)
@@ -11,20 +11,6 @@
 # Wifi enabled for quick installation of prerequisites
 # If you don't use linux much like me, enabling ssh on your pi so you can remote from another computer on your home network is pretty slick.
 # Use linux utility crontab or equivalent to run this script on startup so you don't need a keyboard/monitorself. tutorial here: https://www.dexterindustries.com/howto/auto-run-python-programs-on-the-raspberry-pi/
-
-# Overview of MIDI hardware involved:
-# Xkey 25 usb keyboard (with built-in channel switching)
-# Waldorf Blofeld Synth (16 channels of multi-timbral goodness though this setup only uses 8 channels)
-# Akai MPC1000 (oldskool! Acting as sequencer/brain/laptop replacement running multi-timbral recording mode on JJOSXL)
-# iConnectivity mio 1x1 midi interface (converts midi din ports on mpc to usb for connecting to Pi)
-# DIY Arduino/Teensy Midi 6-button Foot Pedal (cause feet want to perform and manipulate mpc functions too)
-
-# Workflow achieved:
-# Keyboear midi notes can be played and route through mpc1000 and out to Blofeld for realtime synth performance.
-# Keyboard midi notes can be recorded into MPC1000 (mio usb midi cable) running JJOSXL as sequences.
-# MPC1000 midi sequences can play back out to Blofeld as backing tracks or performance where you manipulate blofeld knobs in real-time.
-# MPC1000 can record cc midi or knob movements from blofeld synth (note data filtered out to avoid bad midi loops from keyboard)
-# DIY Arduino/teensy foot pedal buttons each send midi cc values to mpc to control misc. performance functions with my feet in real-time.
 
 from mididings import *
 
